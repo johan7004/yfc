152ac82c4670378962bd69cd9f80319d6cb4bd4d
johan7004 <johanstephen7004@gmail.com>
2022-06-24 20:32:40 +0300

    edit home page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -6,14 +6,29 @@ export default function HomePage() {
   return (
     <>
       <section className="Welcome-message">
-      <NavBar></NavBar>
+        <NavBar></NavBar>
         <div className="Welcome-message__container">
-          <h1 className="Welcome-message__title">Come Let us Know the real God</h1>
+          <h1 className="Welcome-message__title">
+            Come Let us Know the real God
+          </h1>
         </div>
       </section>
       <section className="Bible-study">
         <div className="Bible-study__container">
-          <h1 className="Welcome-message__title">Come Let us Know the real God</h1>
+          <div className="Bible-study__item">
+            <h4 className="Bible-study__text--answer">Answer here Answer here Answer here Answer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer here</h4>
+            <h1 className="Bible-study__text--question">Is There A God, Really??</h1>
+          </div>
+          <div className="Bible-study__item">
+            <h1 className="Bible-study__text--question">
+              Why Do I need To Know about him?
+            </h1>
+            <h4 className="Bible-study__text--answer">Answer here Answer here Answer here Answer here Answer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer here</h4>
+          </div>
+          <div className="Bible-study__item">
+            <h4 className="Bible-study__text--answer">Answer here Answer here Answer here Answer here Answer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer hereAnswer here</h4>
+            <h1 className="Bible-study__text--question">Why Should I Trust Bible not</h1>
+          </div>
         </div>
       </section>
     </>
