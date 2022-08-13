3f0778f5fde600055f3c3fcabc79a5befd25a844
johan7004 <johanstephen7004@gmail.com>
2022-08-14 00:34:13 +0300

    added ping for every 25min for heroku server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./App.css";
-import { Component } from "react";
+import { React, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import BiblePage from "./pages/BiblePage/BiblePage";
 import SongsPage from "./pages/SongsPage/SongsPage";
@@ -10,25 +10,28 @@ import HealthTipsPage from "./pages/HealthTipsPage/HealthTipsPage";
 import BibleStudy from "./pages/BibleStudy/BibleStudy";
 import NavBar from "./components/Navbar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  useEffect(() => {
+    let interval = setInterval(() => {
+      fetch(`https://yfcbackend.herokuapp.com/api/`).then((res) =>
+        console.log(`server Pinged`)
+      );
+    }, 1500000);
+    return () => clearInterval(interval);
+  }, []);
 
-
-
-
-class App extends Component {
-  render() {
-    return (
-      <Routes>
-        <Route path="/" element={<NavBar />}>
-        <Route index element={<HomePage/>}></Route>
-        <Route path="Songs" element={<SongsPage />}></Route>
-        <Route path="bible" element={<BiblePage />}></Route>
-        <Route path="bible-study" element={<BibleStudy />}></Route>
-        <Route path="bible-q-a" element={<BibleQaPage />}></Route>
-        <Route path="health-tips" element={<HealthTipsPage />}></Route>
-        </Route>
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route path="/yfc" element={<NavBar />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="/yfc/Songs" element={<SongsPage />}></Route>
+        <Route path="/yfc/bible" element={<BiblePage />}></Route>
+        <Route path="/yfc/bible-study" element={<BibleStudy />}></Route>
+        <Route path="/yfc/bible-q-a" element={<BibleQaPage />}></Route>
+        <Route path="/yfc/health-tips" element={<HealthTipsPage />}></Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
